af9324f58f5f5a3f8d21e87310ab702f28212f8a
Renan Henrique <renamcomn@Renans-MacBook-Pro.local>
2022-11-01 21:15:16 -0300

    [ADDED] Boilerplate - initial commit

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import Fastify from "fastify";
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient({
+    log: ['query']
+})
+
+async function boostrap() {
+    const fastify = Fastify({
+        logger: true
+    })
+
+    fastify.get('/pools/count', async () => {
+        const count = await prisma.pool.count()
+        return { count }
+    })
+
+    await fastify.listen({ port: 3333 })
+}
+
+boostrap()
